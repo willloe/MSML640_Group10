13dbd765e0b46eed08e0b4d814ee02182dc07f67
wloe-umd <wloe@umd.edu>
2025-12-02 21:27:45 -0500

    bugfix

--- packages/diffusion/src/lora/lora_data.py
+++ packages/diffusion/src/lora/lora_data.py
@@ -98,12 +98,18 @@ def parse_layout_mask(mask: np.ndarray) -> list[tuple[int, int, int, int]]:
     else:
         mask_gray = mask.copy()
 
-    _, mask_bin = cv2.threshold(mask_gray, 250, 255, cv2.THRESH_BINARY_INV)
+    _, mask_bin = cv2.threshold(mask_gray, 200, 255, cv2.THRESH_BINARY)
     contours, _ = cv2.findContours(mask_bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     boxes = []
+    h_img, w_img = mask_gray.shape[:2]
+
     for c in contours:
         x, y, w, h = cv2.boundingRect(c)
-        if w * h < 200:
+        area = w * h
+        if area < 200:
+            continue
+
+        if area > 0.98 * w_img * h_img:
             continue
         boxes.append((x, y, w, h))
 
